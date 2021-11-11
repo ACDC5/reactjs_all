240052d5859d6ad557c2d12e278f34e21fa61937
ACDC5 <ricof6f@gmail.com>
2021-11-11 14:58:11 +0800

    '修复控制台警告-state必须是个对象(之前直接写了个数组)'

---
+++ src/myAxios/MyAxios.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MyAxios extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MyAxios;
--- src/react_extension/8_errorBoundary/Child.js
+++ src/react_extension/8_errorBoundary/Child.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 class Child extends Component {
 
     //模拟服务器返回的正确数据
-    state = [
+    state = {tempData:[
         {'id':'001','name':'tom','age':18},
         {'id':'002','name':'jerry','age':19},
         {'id':'003','name':'div','age':20}
-    ]
+    ]}
 
     /**模拟服务器返回了错误类型的数据，导致渲染失败
      * 触发父组件执行边界错误的执行*/
@@ -16,13 +16,21 @@ class Child extends Component {
 
     //错误必须出现在组件的生命周期函数中，才能被父组件捕获
     render() {
+        const {tempData} = this.state
         return (
             <div>
-                <h1>我是Child<br/>
-                    <h6>想要演示错误边界，切换子组件的state</h6>
+                <h1>
+                    我是Child
+                    <div>
+                        <h6>想要演示错误边界，切换子组件的state</h6>
+                    </div>
                 </h1>
-                {this.state.map(item => {
-                    return <h2 key={item.id}>名称:{item.name}，年龄:{item.age}</h2>
+                {tempData.map(item => {
+                    return (
+                        <h2 key={item.id}>
+                            名称:{item.name}，年龄:{item.age}
+                        </h2>
+                    )
                 })}
             </div>
         );
